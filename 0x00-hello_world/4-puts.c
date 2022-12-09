ae1d6c2935bb229f324971fa597b3bc767bc8f3e
Eto4God <owoyomielisha@yahoo.com>
2022-12-08 17:22:58 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	/* This function prints out text using the printf function */
 	char my_char[] = "with proper grammar, but the outcome is a place of art,\n";
-	
+
 	printf("%s", my_char);
 	return (0);
 }
