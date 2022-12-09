7a082309c2b0bab1ce5ba42de4677623dcd05360
Eto4God <owoyomielisha@yahoo.com>
2022-12-08 16:35:44 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* This function prints out text using the printf function */
+	char my_char[] = "with proper grammar, but the outcome is a place of art,\n";
+	printf("%s", my_char);
+	return (0);
+}
