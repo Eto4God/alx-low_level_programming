9fc9cec88346b4cb47aef77ae54015f2db0715ad
Eto4God <owoyomielisha@yahoo.com>
2022-12-11 09:49:15 -0800

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Program to print numbers to the base of 16 (Hexadecimal) */
+
+	char alphabet;
+	int nums;
+
+	for (nums = 0; nums < 10; nums++)
+		putchar((nums % 10) + '0');
+
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
