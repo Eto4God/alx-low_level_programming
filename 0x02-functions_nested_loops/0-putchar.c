2961e48effc45a6986bf8a801ef9c1d6bb3be7af
Eto4God <owoyomielisha@yahoo.com>
2022-12-13 14:13:55 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * main - prints _putchar followed by a new lines
- * Return: Always 0 (Success)
+ * main - prints putchar
+ * Return: always 0
  */
 
 int main(void)
 {
-	char word[] '=' "_putchar";
-	int counter;
-
-	for (counter = 0; counter < 8; counter++)
-	{
-		_putchar(word(conter));
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
+
