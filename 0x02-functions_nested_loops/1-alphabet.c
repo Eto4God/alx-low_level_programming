ce159bdabbb1cfbd8b844614c0180fc21c55f19c
Eto4God <owoyomielisha@yahoo.com>
2022-12-13 15:26:06 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This function prints the english alphabtets in lower cases
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		-putchar(chr);
+	}
+	-putchar('\n');
+}
