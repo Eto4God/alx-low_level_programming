36f3e349a44f81eaaa1fafffba6855aad5bf3234
Eto4God <owoyomielisha@yahoo.com>
2022-12-13 15:40:47 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - this is a funtion that prints 10 times the alphabet
+ */
+void print_alphabet_x10(void)
+{
+	char chr;
+
+	for (chr = 'a'; chr <= 'z'; chr++)
+	{
+		_putchar(chr);
+	}
+	_putchar('\n');
+}
