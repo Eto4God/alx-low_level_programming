46d30f820f019bd927fa36ec2862c0a73f2e2241
Eto4God <owoyomielisha@yahoo.com>
2023-01-04 12:21:34 -0800

    task 7

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int prime_helper(int n, int d);
+int helper_pal(char *str, int length, int count);
 
 #endif /* MAIN_H */
